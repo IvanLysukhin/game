a42f3919b6402f22c025125bcafda610faa1afa5
Ivan Lysukhin <ilysukhin@mail.ru>
2021-07-19 16:17:21 +0300

    login screen

---
+++ src/components/app/app.jsx
@@ -0,0 +1,30 @@
+function App() {
+    return (
+      <div className="main-container">
+        <section className="login-wrapper">
+          <header className="title-box">
+            <h1 className="title-box__title">Contacts book</h1>
+          </header>
+          <div className="login-form">
+            <form className="login-form__form" action="#" method="post">
+              <ul className="login-form__list">
+                <li className="login-form__item">
+                  <label className="login-form__label" htmlFor="email">E-mail</label>
+                  <input className="login-form__input" id="email" type="email"/>
+                </li>
+                <li className="login-form__item">
+                  <label className="login-form__label" htmlFor="password">Password</label>
+                  <input className="login-form__input" id="password"/>
+                </li>
+                <li className="login-form__item">
+                  <button className="login-form__button" type="submit">Sign in</button>
+                </li>
+              </ul>
+            </form>
+          </div>
+        </section>
+      </div>
+    );
+}
+
+export default App;
